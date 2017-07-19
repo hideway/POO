0a91b886dd4217e93c5b281d45450b5ea13d2ffe
Smart4U <email4webdev@gmail.com>
2017-07-19 16:19:03 +0200

    Add entry point of application

---
+++ app/kernel.php
@@ -0,0 +1,30 @@
+<?php
+
+use function DI\get;
+use function DI\object;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(BASE.'/.env');
+
+$config = [];
+$files = glob(sprintf(CONFIG.'{{,*.}global,{,*.}%s}.php', getenv('APP_ENV') ? : 'production'), GLOB_BRACE);
+foreach ($files as $file) {
+    $config = array_merge($config, include $file);
+}
+$config = new ArrayObject($config, ArrayObject::ARRAY_AS_PROPS);
+
+return [
+    // Config
+    'config' => $config,
+    // Database
+    \Simply\Database\Database::class => object()->constructor(
+        $config->db_name,
+        $config->db_host,
+        $config->db_port,
+        $config->db_user,
+        $config->db_pass,
+        $config->db_options
+    ),
+    'db' => get(\Simply\Database\Database::class),
+];
---
+++ src/simply/BaseApplication.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Simply;
+
+use DI\ContainerBuilder;
+
+/**
+ * Class BaseApplication
+ * @package Simply
+ */
+abstract class BaseApplication {
+
+    protected $app;
+
+    /**
+     * BaseApplication constructor.
+     * @param array $kernel
+     */
+    public function __construct(array $kernel = []) {
+        $this->setContainer($kernel);
+    }
+
+    /**
+     * define the dependency injection container
+     * @param array $kernel
+     */
+    private function setContainer(array $kernel = []) : void {
+        $builder = new ContainerBuilder();
+        $builder->addDefinitions($kernel);
+        $container = $builder->build();
+        $container->set('app', $container);
+        $this->app = $container->get('app');
+    }
+
+    public abstract function run(): void;
+
+}
